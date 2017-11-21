4cdf29fc85d831c7cdcea9292d102513f1b723ce
Ovyerus <Ovyerus@users.noreply.github.com>
2017-11-21 20:49:37 +1100

    bind to specified port

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require('express');
 const app = express();
 const WEBHOOK_ROUTE = '/api/webhook';
 
+app.get('/', (req, res) => {
+    res.send('*dab*');
+});
+
 app.get(WEBHOOK_ROUTE, (req, res) => {
     console.log(req);
     res.sendStatus(200);
 });
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log('hoi');
 });
